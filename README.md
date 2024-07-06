693ac1140eb1c174b000a72366753538b16148df
Rajneesh Kumar <rajneesh.coder@gmail.com>
2024-07-06 23:42:28 +0530

    setup for app

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Resume Builder
+
+---
+## Contact
+- **Email**: kumarrajneesh.work@gmail.com
+- **LinkedIn**: [rajneeshzytox](https://www.linkedin.com/in/rajneeshzytox/)
+- **Instagram**: @rajneeshzytox
+- **Twitter (X)**: rajneeshzytox
+- **Portfolio**: [rajneeshzytox](rajneeshzytox.github.io/portfolio)
---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+
+
+export default {
+  //...
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  
+  // add daisyUI plugin
+  plugins: [require("daisyui")],
+
+  // daisyUI config (optional - here are the default values)
+  daisyui: {
+    themes: [ 
+      "dark",
+      {
+      rajneesh: {
+          "primary": "#1d4ed8",
+          "primary-content": "#cfdefb",
+          "secondary": "#d1d5db",
+          "secondary-content": "#101011",
+          "accent": "#818cf8",
+          "accent-content": "#060715",
+          "neutral": "#6b7280",
+          "neutral-content": "#e0e1e4",
+          "base-100": "#1f2937",
+          "base-200": "#19222e",
+          "base-300": "#141c26",
+          "base-content": "#cdd0d3",
+          "info": "#1d4ed8",
+          "info-content": "#cfdefb",
+          "success": "#22c55e",
+          "success-content": "#000e03",
+          "warning": "#f59e0b",
+          "warning-content": "#150900",
+          "error": "#e11d48",
+          "error-content": "#ffd8d9",
+         },
+      }
+
+    ],
+  },
+
+  //...
+}
